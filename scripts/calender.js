8104c53f8d84d66a2bcde3ff87a68f3114ac491e
garnace <root@darkstar.example.org>
2014-06-16 13:18:29 -0400

    fort

--- scripts/calender.js
+++ scripts/calender.js
@@ -15,22 +15,41 @@ var calStartMD;
 function calendarInit(d){
 	var day;
 
-	if (d ==null)
+/*	if (d ==null)
 		day = new Date();
-	
+
 	calDay = day.getDate();
 	calMonth = day.getMonth();
 	calYear = day.getFullYear();
-	
-	var curMonthStart = new Date(calYear,calMonth,1);
-	calStartMD = curMonthStart.getDay();
+	alert("cal");	
+*/	var curMonthStart = new Date(calYear,calMonth,1);
+//	calStartMD = curMonthStart.getDay();
+	alert("cal");	
+//	$("#cal").append("<table id=\"tabCal\" style=\"width:200;height:100;color:#3344ff;\" border=\"1\" cellspacing=\"2\" cellpadding=\"2\" >");
+	$('<table id="tabCal" width="277" height="100" style="color:#3344ff;" border="1" cellspacing="2" cellpadding="2" ></table>').appendTo('#cal');
+	$('<thead></thead> ').appendTo('#cal table');
 
-	
+	$('<th>Sun</th><th>Mon</th><th>Tues</th><th>Wed</th><th>Thur</th><th>Fri</th><th>Sat</th> ').appendTo("#cal table thead");
+//	$append('</tr></thead><tbody></tbody>').appendTo('#cal');
+	$('<tbody></tbody>').appendTo('#cal');
+	for (var j=0;j<4;j++)
+	{
+		$('<tr>').appendTo('#cal table tbody');		
+		for (var i=0;i<7;i++)
+		{
+		
+			$('<td>hi'+i+'</td>').appendTo('#cal tbody');
+//			$("#cal tbody").append("<td></td>");
+		}//cols
+		$('</tr>').appendTo('#cal tbody');
+	}//rows
+
 }
 
-$(function()(
+$(function(){
+alert("cal");
 calendarInit(new Date());
-
+alert("ccal");
 
 
 
