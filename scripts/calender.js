6f49da019c5c50ad69603d71d5454b87df62527e
garnace <root@darkstar.example.org>
2014-06-16 23:08:21 -0400

    month

--- scripts/calender.js
+++ scripts/calender.js
@@ -8,49 +8,82 @@ var calDay;
 var calMonth;
 var calYear;
 var calStartMD;
-
+//var dd;
 /*
 @param {Object} --date object
 */
 function calendarInit(d){
 	var day;
 
-/*	if (d ==null)
+	if (d ==null)
+	{
+	alert("da  undef");
 		day = new Date();
+	}else{
+		day=d;
+	}
+	alert("da  "+day.toDateString()+"");
+	var calMonth = day.getMonth();
+	var calDay = day.getDate();
+	var calYear = day.getFullYear();
+/*
 
-	calDay = day.getDate();
-	calMonth = day.getMonth();
+
 	calYear = day.getFullYear();
 	alert("cal");	
-*/	var curMonthStart = new Date(calYear,calMonth,1);
+*/
+	var curMonthStart = new Date(calYear,calMonth,1);
+	var curMonthStartDay = curMonthStart.getDay();
 //	calStartMD = curMonthStart.getDay();
-	alert("cal");	
+	alert("calSD"+curMonthStartDay+"::"+curMonthStart.toDateString());	
 //	$("#cal").append("<table id=\"tabCal\" style=\"width:200;height:100;color:#3344ff;\" border=\"1\" cellspacing=\"2\" cellpadding=\"2\" >");
-	$('<table id="tabCal" width="277" height="100" style="color:#3344ff;" border="1" cellspacing="2" cellpadding="2" ></table>').appendTo('#cal');
+//	$('<table id="tabCal" width="277" height="100" style="color:#3344ff;" border="1" cellspacing="2" cellpadding="2" ></table>').appendTo('#cal');
+	$('<table id="tabCal"  style="color:#3344ff;" border="1" cellspacing="2" cellpadding="2" ></table>').appendTo('#cal');
 	$('<thead></thead> ').appendTo('#cal table');
 
 	$('<th>Sun</th><th>Mon</th><th>Tues</th><th>Wed</th><th>Thur</th><th>Fri</th><th>Sat</th> ').appendTo("#cal table thead");
 //	$append('</tr></thead><tbody></tbody>').appendTo('#cal');
-	$('<tbody></tbody>').appendTo('#cal');
+	$('<tbody></tbody>').appendTo('#cal table');
+	var startMCount=0;
 	for (var j=0;j<4;j++)
 	{
-		$('<tr>').appendTo('#cal table tbody');		
+//		$('<tr></tr>').appendTo('#cal table tbody');		
+		$('<tr></tr>').appendTo('#cal table tbody');		
 		for (var i=0;i<7;i++)
 		{
 		
-			$('<td>hi'+i+'</td>').appendTo('#cal tbody');
-//			$("#cal tbody").append("<td></td>");
+//			$('<td>hi'+i+'</td>').appendTo('#cal tbody');
+			if (curMonthStartDay == i && j==0)
+			{
+			$("#cal table tbody tr:eq("+j+")").append('<td>hi'+(startMCount++)+'</td>');
+			}
+			else if (startMCount > 0)
+			{
+			if(calDay==startMCount)//check current day
+				$("#cal table tbody tr:eq("+j+")").append('<td><span style="opacity:0.5;">no'+(startMCount++)+'</span></td>');
+			else
+				$("#cal table tbody tr:eq("+j+")").append('<td>hi'+(startMCount++)+'</td>');
+			}
+
+			else 
+			{
+			$("#cal table tbody tr:eq("+j+")").append('<td>hi</td>');
+			}
+
+//			$("#cal table").append('<td>hi'+i+'</td>');
 		}//cols
-		$('</tr>').appendTo('#cal tbody');
+//		$('</tr>').appendTo('#cal table tbody');
 	}//rows
 
 }
 
 $(function(){
-alert("cal");
-calendarInit(new Date());
+$(document).ready(function(){
+var dd= new Date();
+alert("cal"+dd);
+calendarInit(dd);
 alert("ccal");
 
-
+});//doc ready
 
 });
