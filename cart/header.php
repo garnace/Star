1024c74ac9be9a43e21370a062360667d2c38cce
garnace <root@darkstar.example.org>
2015-02-07 00:16:08 -0500

    uf07

--- cart/header.php
+++ cart/header.php
@@ -2,10 +2,11 @@
 <!--CTYPE html-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1"/>
+
 
 <head>
 <title>StarAdvisor</title>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1"/>
 <link href="../css/style.css" rel="stylesheet" type="text/css" title="starA">
 <link href="../cf/theme/css/less-style.css" rel="stylesheet" type="text/css">
 <link href="../cf/theme/css/bootstrap.css" rel="stylesheet" type="text/css" title="starA">
