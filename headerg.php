b1f049c6b3cd99d6c222550e4dfc47721dd27bbd
garnace <root@darkstar.example.org>
2015-08-13 21:11:30 -0400

    ua13

--- headerg.php
+++ headerg.php
@@ -196,7 +196,30 @@ function getM()
 <!--do on load events-->
 
 <body onload="load();"  >
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '937681792957711',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
 
 
     <!--TWITTER BOOTSTRAP TUTORIAL</h1-->
