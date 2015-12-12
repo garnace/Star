b903f20a1bb5864ed97663cc7932072f9ab090c5
garnace <root@darkstar.example.org>
2015-12-11 22:11:32 -0500

    quidj

--- scripts/script.js
+++ scripts/script.js
@@ -4159,7 +4159,7 @@ function readFooStar(dbId)
 
 //	$.ajax({url:"http://"+hostUrl+"/api/v1/recipes?"+ranInput, dataType:'json'}).done(function(data){
 	$.ajax({url:"http://"+hostUrl+"/StarAdvisor/model/recipeStar.json", dataType:'json'}).done(function(data){
-		alert("data:"+eval(data));
+		alert("data:"+data.length+"items");
 		$.each(data,function(i,item){
 			alert(item.ingredients[0]+"hi"+JSON.stringify(item)+"");
 		});//each json
